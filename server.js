a241ecbbb6180d78c8c95721eeb1f7d31e280c34
Dickson <dickson1611.moreno@gmail.com>
2024-09-09 18:50:01 -0500

    Añadido servidor Express para servir archivos estáticos de Vite

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Serve index.html for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
