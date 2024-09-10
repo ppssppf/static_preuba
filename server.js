7099681c868d15e40745ceb57464f65bc1dd49bf
Dickson <dickson1611.moreno@gmail.com>
2024-09-09 19:10:46 -0500

    Actualiza configuración para usar módulos ESM o CommonJS

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Serve static files from the build directory
 app.use(express.static(path.join(__dirname, 'dist')));
